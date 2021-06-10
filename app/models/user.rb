8df40c5c8e119a998bee51c2710b78185d69183b
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-10 18:51:23 +0900

    gimeiの導入、fakerでユーザー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :birth_id
+    validates :birth_date
     with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' } do
-      validates :family_name
       validates :first_name
+      validates :last_name
     end
     with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' } do
-      validates :family_name_kana
+      validates :last_name_kana
       validates :first_name_kana
     end
     PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i.freeze
