85631b8b49a20d23ad27504ac56a437f09be3ab4
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-10 18:21:31 +0900

    バリデーションの追加,rspecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+  with_options presence: true do
+    validates :nickname
+    validates :birth_id
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' } do
+      validates :family_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+    PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには半角英字と半角数字の両方を含めて設定してください'
+  end
+  end
