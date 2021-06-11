ca2aea906648d55ce5050ba8f34fc7eb53ac8cfa
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-11 17:03:21 +0900

    Activehashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :product
+    validates :information
+    validates :images
+    validates :selling_price
+  end
+  validates :selling_price, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
+  validates :selling_price, format: { with: /\A[0-9]+\z/, message: '半角数字で入力して下さい' }
 end
