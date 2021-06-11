c7996dd49580030ff317f091d5e4d605a60eafa8
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-11 18:09:54 +0900

    コントローラーの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,17 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_fee
+  belongs_to :status
+  belongs_to :fee
   belongs_to :prefecture
-  belongs_to :days_to_ship
-  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 }
+  belongs_to :schedule
+  validates :category_id, :status_id, :fee_status_id, :prefecture_id, :schedule_delivery_id, numericality: { other_than: 1 }
   with_options presence: true do
-    validates :product
-    validates :information
-    validates :images
-    validates :selling_price
+    validates :name
+    validates :info
+    validates :price
   end
-  validates :selling_price, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
-  validates :selling_price, format: { with: /\A[0-9]+\z/, message: '半角数字で入力して下さい' }
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
+  validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字で入力して下さい' }
 end
