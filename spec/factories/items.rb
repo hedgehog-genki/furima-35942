f564e9c7c6b348e6b6a9951f5cd060b9bc5cac28
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-14 21:24:11 +0900

    factorybotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,8 +10,5 @@ FactoryBot.define do
     schedule_delivery_id { 2 }
     price { 10000 }
     association :user
-    after(:build) do |item|
-      item.image.attach(io: File.open("public/images/test_image.png"), filename: "test_image.png")
-    end
   end
 end
