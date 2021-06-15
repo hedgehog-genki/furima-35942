f0d41be4d521d554f15c02ec8bd6d02ebdd72bde
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-15 11:03:37 +0900

    バリデーションの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :schedule
   validates :category_id, :status_id, :fee_status_id, :prefecture_id, :schedule_delivery_id, numericality: { other_than: 1 }
-  validates :name, presence: true
-  validates :info, presence: true
-  validates :price, presence: true
-  validates :images, presence: true
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :price
+    validates :images
+  end
   validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
   validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字で入力して下さい' }
 end
