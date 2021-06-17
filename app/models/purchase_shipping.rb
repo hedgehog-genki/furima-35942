6a48229e732c4ca1a5082f4b2edff253bc957e22
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-17 15:58:03 +0900

    購入機能の実装

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,20 +1,19 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :item_id, :user_id, :token
+  attr_accessor :postal_coad, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id, :token
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :postal_coad, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :phone_number, format: { with: /\A\d{11}\z/, message: "PhoneNumber must be 11 digit Half-width numbers" }
     validates :city
     validates :address
     validates :token
-    validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   end
 
   def save
-    purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Shipping.create(postal_code: postal_code, prefecture_id: prefecture, city: city, address: address,
-                    building: building, phone_number: phone_number, purchase_id: purchase.id)
+    @purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Shipping.create(postal_coad: postal_coad, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: @purchase.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_071314) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
-    t.string "building", null: false
+    t.string "building"
     t.string "phone_number", null: false
     t.bigint "purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
